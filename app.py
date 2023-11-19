09b8fbf163ce0f243932d5b81aa32169fe775645
dgna <@>
2023-11-19 23:28:08 +0900

    Application factory

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def hello_world():
+        return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run()
+    return app
