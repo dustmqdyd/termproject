25ac0c058ca60caa5491906b4bdd9a16e611a024
dgna <@>
2023-12-03 09:19:01 +0900

    Add blueprint

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
-
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello World!'
+    import main_views
+    app.register_blueprint(main_views.bp)
 
     return app
